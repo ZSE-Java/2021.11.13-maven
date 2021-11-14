5fb8286820a9ad0437f8964083a95ef70c90df42
Mateusz <mat.bereda@gmail.com>
2021-11-14 19:28:22 +0100

    init

---
+++ src/main/java/pl/edu/zse/App.java
@@ -0,0 +1,7 @@
+package pl.edu.zse;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/edu/zse/Car.java
@@ -0,0 +1,22 @@
+package pl.edu.zse;
+
+public class Car {
+    private String brand;
+    private String model;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
